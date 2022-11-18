0ab0b16a2e4db58e6473fcff753076a16dc602c5
Kayky Babosa <kaykybarbosa3772@gmail.com>
2022-11-18 00:23:30 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-function App() {
-  return(
-    <h1>Olá mundo!</h1>
+import NotificationButton from "./components/NotificationButton"
 
+function App() {
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
-  
 }
 
-export default App
+export default App;
