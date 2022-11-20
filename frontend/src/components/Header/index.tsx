0d119d28ed1b7b09c10468ce664459af745efec2
Kayky Babosa <kaykybarbosa3772@gmail.com>
2022-11-20 07:38:19 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src= {logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvovindo por
+          <a href="https://www.instagram.com/preto_kayky/">@preto_kayky</a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -4,9 +4,11 @@ import './styles.css'
 
 function NotificationButton() {
   return (
-    <div className="dsmata-red-btn">
-      <img src={icon} alt="Notificar" />
-    </div>
+    <>
+      <div className="dsmata-red-btn">
+        <img src= {icon} alt="Notificar" />
+      </div>
+    </>
   );
 }
 
